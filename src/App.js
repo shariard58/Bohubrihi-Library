bed5f92843d21d04ef0cece29b15d7823755c483
Shariar Mahmud Duke <shariard58@gmail.com>
2021-12-16 17:15:29 +0600

    Till the second video Component Tree

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 
+import Maincomponent from './components/MainComponent';
+
 function App() {
   return (
     <div className="App">
+
+      <Maincomponent />
 
     </div>
   );
